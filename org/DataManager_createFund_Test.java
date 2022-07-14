d2fdb146b291758f39f6ab979affd6e1b4aa61a2
HarshSareen <43045653+HarshSareen@users.noreply.github.com>
2022-07-14 20:54:42 +0530

    Updated data entries, added a line in the writeup and removed the throws Exception thing

--- org/DataManager_createFund_Test.java
+++ org/DataManager_createFund_Test.java
@@ -1,87 +1,87 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.util.Map;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class DataManager_createFund_Test {
-	
-	/*
-	 * This is a test class for the DataManager.createFund method.
-	 * Add more tests here for this method as needed.
-	 * 
-	 * When writing tests for other methods, be sure to put them into separate
-	 * JUnit test classes.
-	 */
 
-	@Test
-	public void testSuccessfulCreation() {
+    /*
+     * This is a test class for the DataManager.createFund method.
+     * Add more tests here for this method as needed.
+     *
+     * When writing tests for other methods, be sure to put them into separate
+     * JUnit test classes.
+     */
 
-		DataManager dm = new DataManager(new WebClient("localhost", 3001) {
-			
-			@Override
-			public String makeRequest(String resource, Map<String, Object> queryParams) {
-				return "{\"status\":\"success\",\"data\":{\"_id\":\"12345\",\"name\":\"new fund\",\"description\":\"this is the new fund\",\"target\":10000,\"org\":\"5678\",\"donations\":[],\"__v\":0}}";
-				
-			}
-			
-		});
-		
-		
-		Fund f = dm.createFund("12345", "new fund", "this is the new fund", 10000);
-		
-		assertNotNull(f);
-		assertEquals("this is the new fund", f.getDescription());
-		assertEquals("12345", f.getId());
-		assertEquals("new fund", f.getName());
-		assertEquals(10000, f.getTarget());
-		
-	}
-	
-	/*
-	 * If status is not "success" then fund should be null
-	 */
-	@Test
-	public void testUnSuccessfulCreation() {
+    @Test
+    public void testSuccessfulCreation() {
 
-		DataManager dm = new DataManager(new WebClient("localhost", 3001) {
-			
-			@Override
-			public String makeRequest(String resource, Map<String, Object> queryParams) {
-				return "{\"status\":\"error\",\"data\":{\"_id\":\"12345\",\"name\":\"new fund\",\"description\":\"this is the new fund\",\"target\":10000,\"org\":\"5678\",\"donations\":[],\"__v\":0}}";
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
 
-			}
-			
-		});
-		
-		
-		Fund f = dm.createFund("12345", "new fund", "this is the new fund", 10000);
-		
-		assertNull(f); // Since the status response is error then this fund should be null
-		
-	}
-	
-	/*
-	 * Should cover the Exception thrown code
-	 */
-	@Test
-	public void testExceptionThrown() {
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                return "{\"status\":\"success\",\"data\":{\"_id\":\"12345\",\"name\":\"new fund\",\"description\":\"this is the new fund\",\"target\":10000,\"org\":\"5678\",\"donations\":[],\"__v\":0}}";
 
-		DataManager dm = new DataManager(new WebClient("localhost", 3001) {
-			
-			@Override
-			public String makeRequest(String resource, Map<String, Object> queryParams) throws Exception {
-				throw new Exception();
-			}
-			
-		});
-		
-		
-		Fund f = dm.createFund("12345", "new fund", "this is the new fund", 10000);
-		
-		assertNull(f); // Since the status response is error then this fund should be null
-		
-	}
+            }
+
+        });
+
+
+        Fund f = dm.createFund("12345", "new fund", "this is the new fund", 10000);
+
+        assertNotNull(f);
+        assertEquals("this is the new fund", f.getDescription());
+        assertEquals("12345", f.getId());
+        assertEquals("new fund", f.getName());
+        assertEquals(10000, f.getTarget());
+
+    }
+
+    /*
+     * If status is not "success" then fund should be null
+     */
+    @Test
+    public void testUnSuccessfulCreation() {
+
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                return "{\"status\":\"error\",\"data\":{\"_id\":\"12345\",\"name\":\"new fund\",\"description\":\"this is the new fund\",\"target\":10000,\"org\":\"5678\",\"donations\":[],\"__v\":0}}";
+
+            }
+
+        });
+
+
+        Fund f = dm.createFund("12345", "new fund", "this is the new fund", 10000);
+
+        assertNull(f); // Since the status response is error then this fund should be null
+
+    }
+
+    /*
+     * Should cover the Exception thrown code
+     */
+    @Test
+    public void testExceptionThrown() {
+
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                throw new IllegalArgumentException();
+            }
+
+        });
+
+
+        Fund f = dm.createFund("12345", "new fund", "this is the new fund", 10000);
+
+        assertNull(f); // Since the status response is error then this fund should be null
+
+    }
 
 
 }
